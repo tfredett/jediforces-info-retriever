b7014016b0d5c3f10aaf0ea8612f4ee96350a1b1
Tim Fredette <tfredett@gmail.com>
2020-07-06 19:27:27 -0400

    Add private message versions of current commands. Add additional text to make it more broad

--- jediforces_bot.rb
+++ jediforces_bot.rb
@@ -10,11 +10,19 @@ Dotenv.load('config.env.local', 'main.env')
 @rate_limiter.bucket(:message, limit: 3, time_span: 60, delay: 5)
 
 @bot.message(with_text: '!server_info') do |event|
+  event.author.pm ENV['config.server_info']
+end
+
+@bot.message(with_text: '!server_rules') do |event|
+  event.author.pm ENV['config.server_rules']
+end
+
+@bot.message(with_text: '!server_info_broadcast') do |event|
   next if @rate_limiter.rate_limited?(:message, event.channel)
   event.respond ENV['config.server_info']
 end
 
-@bot.message(with_text: '!server_rules') do |event|
+@bot.message(with_text: '!server_rules_broadcast') do |event|
   next if @rate_limiter.rate_limited?(:message, event.channel)
   event.respond ENV['config.server_rules']
 end
