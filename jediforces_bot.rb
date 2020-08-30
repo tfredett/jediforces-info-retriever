b1e3c8a392619d732a51c02d4b9ca45e930d8c3d
Tim Fredette <tfredett@gmail.com>
2020-08-29 20:21:15 -0400

    Add in rate limited command for Noobmeister

--- jediforces_bot.rb
+++ jediforces_bot.rb
@@ -9,6 +9,9 @@ Dotenv.load('config.env.local', 'main.env')
 @rate_limiter = Discordrb::Commands::SimpleRateLimiter.new
 @rate_limiter.bucket(:message, limit: 3, time_span: 60, delay: 5)
 
+@noob_word_rate_limiter = Discordrb::Commands::SimpleRateLimiter.new
+@noob_word_rate_limiter.bucket(:message, limit: 1, time_span: 90)
+
 @bot.message(with_text: '!server_info') do |event|
   event.author.pm ENV['config.server_info']
 end
@@ -25,6 +28,11 @@ end
 @bot.message(with_text: '!server_rules_broadcast') do |event|
   next if @rate_limiter.rate_limited?(:message, event.channel)
   event.respond ENV['config.server_rules']
+end
+
+@bot.message(with_text: '!noob_hacker') do |event|
+  next if @noob_word_rate_limiter.rate_limited?(:message, event.channel)
+  event.respond 'Noobmeister: You Hacker!'
 end
 
 @bot.run
